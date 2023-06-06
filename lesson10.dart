063616d4d4afaf4352e10a2d9e7c896b094441f6
sezimka123 <erali.sezim@gmail.com>
2023-06-06 16:24:58 +0600

    first commit

---
+++ lesson10.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void main() {
+   print("Enter the number from 1 to 100 so that computer could guess it: ");
+   int input = int.parse(stdin.readLineSync()!);
+binarySearch();
+}
+
+binarySearch() {
+  int min = 1;
+  int max = 100;
+  int count = 0;
+
+  while (min <= max) {
+    int middle = (min + max) ~/2;
+    count++;
+    print("Is it $middle?\n1) Yes\n2) Greater\n3) Less");
+    int answer = int.parse(stdin.readLineSync()!);
+    if (answer == 1) {
+      print("Got it in $count steps!");
+      break;
+    } else if (answer == 2) {
+      min = middle;
+    } else if (answer == 3) {
+      max = middle;
+    } else {
+      print("No such option");
+    }
+  }
+  
+  
+}
